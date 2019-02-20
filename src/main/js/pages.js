a5e71a9589ed94413d7de1dfb7dbd6ee43b63f03
Caleb DeHaan <calebdehaan@gmail.com>
2019-02-19 18:07:04 -0600

    added a navbar on the top, css'ed a bit in order to make it look not ugly

--- src/main/js/pages.js
+++ src/main/js/pages.js
@@ -7,6 +7,7 @@ import * as Users from 'js/users';
 import * as Login from 'js/login';
 import axios from 'axios';
 
+
 export class Home extends React.Component {
 	constructor(props){
 		super(props);
@@ -30,17 +31,41 @@ export class Home extends React.Component {
 	render() {
 		return (
 			<div className="container padded">
-				<h1>BearSitters</h1>
-				{this.state.message} This is the home page.
+                <div>
+                    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                        <a className="navbar-brand" href="#">BearSitters</a>
+                        <button className="navbar-toggler" type="button" data-toggle="collapse"
+                                data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                                aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
 
-				<ul >
-					<li><Link to="/register">Register</Link></li>
-					<li><Link to="/login">Login</Link></li>
-					<li><Link to="/page-1">Page 1</Link></li>
-					<li><Link to="/page-2">Page 2</Link></li>
-					<li><Link to="/page-3">Page 3</Link></li>
-					<li><Link to="/">Homepage</Link></li>
-				</ul>
+                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul className="navbar-nav mr-auto">
+                                <li className="nav-item active">
+                                    <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#/register">Register</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#/login">Login</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#/page-1">Page1</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#/page-2">Page2</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#/page-3">Page3</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </nav>
+                </div>
+				{this.state.message} This is the home page. <br/>
+				<img src={require('../resources/statics/bearsitting.png')} alt="the bear will sit" title="the bear will sit"/>
 			</div>
 		);
 	}
@@ -50,6 +75,41 @@ export class RegisterPage extends React.Component {
 	render() {
 		return (
 			<div className="container padded">
+
+                <div>
+                    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                        <a className="navbar-brand" href="#">BearSitters</a>
+                        <button className="navbar-toggler" type="button" data-toggle="collapse"
+                                data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                                aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+
+                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul className="navbar-nav mr-auto">
+                                <li className="nav-item active">
+                                    <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#/register">Register</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#/login">Login</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#/page-1">Page1</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#/page-2">Page2</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#/page-3">Page3</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </nav>
+                </div>
+
 				<div className="row">
 					<div className="col-6 offset-md-3">
 						<h2>Register</h2>
@@ -57,7 +117,6 @@ export class RegisterPage extends React.Component {
 						<Login.RegistrationForm />
 					</div>
 				</div>
-                <Link to="/">Homepage</Link>
 			</div>
 		);
 	}
@@ -67,6 +126,41 @@ export class LoginPage extends React.Component {
 	render() {
 		return (
 			<div className="container padded">
+
+                <div>
+                    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                        <a className="navbar-brand" href="#">BearSitters</a>
+                        <button className="navbar-toggler" type="button" data-toggle="collapse"
+                                data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                                aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+
+                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul className="navbar-nav mr-auto">
+                                <li className="nav-item active">
+                                    <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#/register">Register</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#/login">Login</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#/page-1">Page1</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#/page-2">Page2</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#/page-3">Page3</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </nav>
+                </div>
+
 				<div className="row">
 					<div className="col-6 offset-md-3">
 						<h2>Login</h2>
@@ -74,7 +168,6 @@ export class LoginPage extends React.Component {
 						<Login.LoginForm />
 					</div>
 				</div>
-                <Link to="/">Homepage</Link>
 			</div>
 		);
 	}
@@ -84,6 +177,41 @@ class Page1 extends React.Component {
 	render() {
 		return (
 			<div className="container padded">
+
+                <div>
+                    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                        <a className="navbar-brand" href="#">BearSitters</a>
+                        <button className="navbar-toggler" type="button" data-toggle="collapse"
+                                data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                                aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+
+                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul className="navbar-nav mr-auto">
+                                <li className="nav-item active">
+                                    <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#/register">Register</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#/login">Login</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#/page-1">Page1</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#/page-2">Page2</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#/page-3">Page3</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </nav>
+                </div>
+
 				This is page 1.
 
 				{ _.isDefined(this.props.authentication) &&
@@ -93,7 +221,6 @@ class Page1 extends React.Component {
 				{ _.isDefined(this.props.user) &&
 				<div>Welcome, {this.props.user.principal}!</div>
 				}
-                <Link to="/">Homepage</Link>
 			</div>
 		);
 	}
@@ -112,8 +239,42 @@ export class Page2 extends React.Component {
 	render() {
 		return (
 			<div className="container padded">
+
+                <div>
+                    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                        <a className="navbar-brand" href="#">BearSitters</a>
+                        <button className="navbar-toggler" type="button" data-toggle="collapse"
+                                data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                                aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+
+                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul className="navbar-nav mr-auto">
+                                <li className="nav-item active">
+                                    <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#/register">Register</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#/login">Login</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#/page-1">Page1</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#/page-2">Page2</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#/page-3">Page3</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </nav>
+                </div>
+
 				This is page 2.
-                <Link to="/">Homepage</Link>
 			</div>
 		);
 	}
@@ -123,8 +284,42 @@ export class Page3 extends React.Component {
 	render() {
 		return (
 			<div className="container padded">
+
+                <div>
+                    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                        <a className="navbar-brand" href="#">BearSitters</a>
+                        <button className="navbar-toggler" type="button" data-toggle="collapse"
+                                data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                                aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+
+                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul className="navbar-nav mr-auto">
+                                <li className="nav-item active">
+                                    <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#/register">Register</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#/login">Login</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#/page-1">Page1</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#/page-2">Page2</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#/page-3">Page3</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </nav>
+                </div>
+
 				This is page 3.
-                <Link to="/">Homepage</Link>
 			</div>
 		);
 	}
