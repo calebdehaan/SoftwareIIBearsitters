c783fe277eb34abedafccec426b99f52b0819942
Neil <neil@neilwilcoxson.com>
2019-03-04 13:18:55 -0600

    Test files commented out

--- src/test/java/petfinder/site/test/unit/UserTest.java
+++ src/test/java/petfinder/site/test/unit/UserTest.java
@@ -1,38 +1,38 @@
-package petfinder.site.test.unit;
-
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
-
-public class UserTest {
-  @DisplayName("Sitter")
-  @Nested
-  class Sitter {
-    @DisplayName("Constructor")
-    @Nested
-    class Constructor {
-
-    }
-
-    @DisplayName("Setters")
-    @Nested
-    class Setters {
-
-    }
-  }
-
-  @DisplayName("Owner")
-  @Nested
-  class Owner {
-    @DisplayName("Constructor")
-    @Nested
-    class Constructor {
-
-    }
-
-    @DisplayName("Setters")
-    @Nested
-    class Setters {
-
-    }
-  }
-}
+//package petfinder.site.test.unit;
+//
+//import org.junit.jupiter.api.DisplayName;
+//import org.junit.jupiter.api.Nested;
+//
+//public class UserTest {
+//  @DisplayName("Sitter")
+//  @Nested
+//  class Sitter {
+//    @DisplayName("Constructor")
+//    @Nested
+//    class Constructor {
+//
+//    }
+//
+//    @DisplayName("Setters")
+//    @Nested
+//    class Setters {
+//
+//    }
+//  }
+//
+//  @DisplayName("Owner")
+//  @Nested
+//  class Owner {
+//    @DisplayName("Constructor")
+//    @Nested
+//    class Constructor {
+//
+//    }
+//
+//    @DisplayName("Setters")
+//    @Nested
+//    class Setters {
+//
+//    }
+//  }
+//}
