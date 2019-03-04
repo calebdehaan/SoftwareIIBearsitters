e19cc5d8183fb0532d39490c9abd627d8d0e6c5a
Neil <neil@neilwilcoxson.com>
2019-03-04 13:02:31 -0600

    Added test skeleton for Pet and User

---
+++ src/test/java/petfinder/site/test/unit/PetTest.java
@@ -0,0 +1,18 @@
+package petfinder.site.test.unit;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+
+public class PetTest {
+  @DisplayName("Constructor")
+  @Nested
+  class Constructor {
+
+  }
+
+  @DisplayName("Setters")
+  @Nested
+  class Setters {
+
+  }
+}
