cd19ad1cc79de5d01d6379d9bc7714bb43f53e97
ECS\JONESAL <ajones.mvd@gmail.com>
2019-04-13 22:02:48 -0500

    Working on nav bar functionality

--- src/main/js/Common/navBar.js
+++ src/main/js/Common/navBar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import * as Users from '../User/users';
 import {connect} from 'react-redux';
 import Favicon from 'react-favicon';
@@ -17,19 +18,27 @@ class NavBar extends React.Component {
 		return (
 			<div>
 				<Favicon url="https://img.icons8.com/ios/50/000000/bear-footprint-filled.png" />
-				<nav className="navbar navbar-expand-lg navbar-dark bg-dark" >
-					<a className="navbar-brand" href="#">BearSitters</a>
-					<button className="navbar-toggler" type="button" data-toggle="collapse"
+				<nav class="navbar navbar-expand-lg navbar-dark bg-dark" >
+					<a class="navbar-brand" href="#">BearSitters</a>
+					<button class="navbar-toggler" type="button" data-toggle="collapse"
 							data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
 							aria-expanded="false" aria-label="Toggle navigation">
-						<span className="navbar-toggler-icon"></span>
+						<span class="navbar-toggler-icon"></span>
 					</button>
-
-					<div className="collapse navbar-collapse" id="navbarSupportedContent">
-						<ul className="navbar-nav mr-auto">
-							<li className="nav-item active">
-								<a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+					<div class="collapse navbar-collapse" id="navbarNav">
+						<ul class="navbar-nav">
+							<li class="nav-item active">
+								<a class="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
 							</li>
+							<li class="nav-item">
+                                <a class="nav-link" href="#/login"> </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#/register"> </a>
+                            </li>
+
+
+
 
 
 							{ (this.props.user != null) && (this.props.user.roles != null) && (this.props.user.roles.includes('OWNER')) &&
